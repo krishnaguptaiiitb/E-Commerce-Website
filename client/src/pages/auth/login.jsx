20886055d56abc3540c5855ce19ed6443c76ef80
krishnaguptaiiitb <krishnaguptaiiitb@gmail.com>
2025-10-30 15:05:40 +0530

    Fix import case for CommonForm component

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,4 +1,4 @@
-import CommonForm from "@/components/common/Form";
+import CommonForm from "@/components/common/form";
 import { loginFormControls } from "@/config";
 import { toast } from "@/hooks/useToast";
 import { loginUser } from "@/store/auth-slice";
